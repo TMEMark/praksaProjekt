285e91242029e2ddaa1ce02bbf1936fa556babef
TMEMark <markobuljan.os@gmail.com>
2021-07-23 13:01:02 +0200

    crud and connection on database

--- server/company.sql
+++ server/company.sql
@@ -1,25 +1,24 @@
--- create database Conmpany;
--- drop database Conmpany;
--- create database company;
--- use company;
+ create database company;
+ use company;
 
-create table login (
-    loginNo int IDENTITY(1,1) PRIMARY KEY,
-    loginUserName varchar(20),
-    loginPassword varchar(20)
+ create table login (
+     loginNo int not null PRIMARY KEY auto_increment,
+     loginUserName varchar(20),
+     loginPassword varchar(20)
+ );
+
+ create table department (
+     departmentNo int not null PRIMARY KEY auto_increment,
+     departmentName varchar(20),
+     departmentLocation varchar(20)
+ );
+
+ create table employee (
+     employeeNo int not null PRIMARY KEY auto_increment,
+     employeename varchar(50),
+     salary int,
+     departmentNo int references department(departmentNo),
+     lastModifyDate datetime
 );
 
-create table department (
-    departmentNo int IDENTITY(1,1) PRIMARY KEY,
-    departmentName varchar(20),
-    departmentLocation varchar(20)
-);
-
-create table employee (
-    employeeNo int IDENTITY(1,1) PRIMARY KEY,
-    employeename varchar(50),
-    salary int,
-    departmentNo int references department(departmentNo),
-    lastModifyDate datetime
-);
-
+select * from department
--- server/index.js
+++ server/index.js
@@ -1,20 +1,31 @@
-// Izrada express servera
-const express = require('express');
+const express = require("express");
+const bodyParser = require('body-parser')
+const cors = require('cors')
 const app = express();
-const mysql = require('mysql');
+const sql = require("mysql");
 
-const db = mysql.createPool({
-    host:'localhost',
-    user: 'root',
-    password: 'password',
-    database: 'company',
-})
-
-app.get('/', (req,res) => {
-    res.send("hello world");
+const db = sql.createPool({
+    host: "localhost",
+    user: "root",
+    password: "password",
+    database: "company"
 });
 
-//Server na portu 3001
+app.use(cors());
+app.use(express.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.post("/api/insert", (req, res) => {
+
+    const departmentName = req.body.departmentName
+    const departmentLocation = req.body.departmentLocation
+
+    const sqlInsert = "INSERT INTO department (departmentName, departmentLocation) VALUES (?,?);";
+    db.query(sqlInsert, [departmentName, departmentLocation] ,(err, result) => {console.log(err);
+    
+    });
+});
+
 app.listen(3001, () => {
-    console.log("running on port 3001");
+    console.log("running on port 3001")
 });
